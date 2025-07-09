7984395f39600adff0e6ca4b61ea2ebd5986e5ed
Paul K <102385105+codingcapy@users.noreply.github.com>
2025-07-08 22:04:20 -0700

    add query client to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,47 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-
-// Import the generated route tree
+import { Link, RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import Auth from "./components/auth/Auth";
+import { QueryClient } from "@tanstack/react-query";
 
-// Create a new router instance
-const router = createRouter({ routeTree });
+export const queryClient = new QueryClient();
 
-// Register the router instance for type safety
+const router = createRouter({
+    routeTree,
+    context: { queryClient },
+    defaultNotFoundComponent: () => {
+        return (
+            <div className="flex flex-col min-h-screen">
+                <main className="flex-1 bg-[#040406] text-white p-3 pt-[100px]">
+                    <p className="text-white text-xl md:text-4xl text-center pt-10 md:pt-32">
+                        Whoops! This isn't what you're looking for 😅
+                    </p>
+                    <div className="flex flex-col my-10 md:my-20 mx-auto w-[250px]">
+                        <Link
+                            to="/"
+                            className="py-2 px-10 rounded bg-cyan-600 text-center tracking-widest"
+                        >
+                            LET'S GO HOME
+                        </Link>
+                    </div>
+                </main>
+            </div>
+        );
+    },
+});
+
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+    interface Register {
+        router: typeof router;
+    }
 }
 
-// Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <Auth>
-      <RouterProvider router={router} />
-    </Auth>
-  );
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <Auth>
+            <RouterProvider router={router} />
+        </Auth>
+    );
 }
